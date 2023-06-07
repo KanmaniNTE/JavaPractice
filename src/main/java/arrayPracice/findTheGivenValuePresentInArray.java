e520ec40e98f442c1eab3b495e2dfd3407baf0c0
Kanmani <nte.kanmani31@gmail.com>
2023-06-07 23:03:44 +0530

    Array Asending

--- src/main/java/arrayPracice/findTheGivenValuePresentInArray.java
+++ src/main/java/arrayPracice/findTheGivenValuePresentInArray.java
@@ -2,7 +2,7 @@ package arrayPracice;
 
 public class findTheGivenValuePresentInArray {
 	int c[] = { 10, 20, 30, 40, 50, 60 };
-	int b = 70;
+	int b = 60;
 	boolean givenValue= false;
 
 	public static void main(String[] args) {
